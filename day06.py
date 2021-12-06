13805a37d5f9deaa249d979ddad4dfc4e0649c1b
Teekeks <info@teawork.de>
2021-12-06 10:17:05 +0100

    without list copy

--- day06.py
+++ day06.py
@@ -12,16 +12,16 @@ for sushi in raw_fish:
     fish[sushi] += 1
 
 for day in range(256):
-    c_fish = fish.copy()
-    fish[0] = c_fish[1]
-    fish[1] = c_fish[2]
-    fish[2] = c_fish[3]
-    fish[3] = c_fish[4]
-    fish[4] = c_fish[5]
-    fish[5] = c_fish[6]
-    fish[6] = c_fish[7] + c_fish[0]
-    fish[7] = c_fish[8]
-    fish[8] = c_fish[0]
+    a = fish[0]
+    fish[0] = fish[1]
+    fish[1] = fish[2]
+    fish[2] = fish[3]
+    fish[3] = fish[4]
+    fish[4] = fish[5]
+    fish[5] = fish[6]
+    fish[6] = fish[7] + a
+    fish[7] = fish[8]
+    fish[8] = a
 
     if day == 79:
         print('Part 1:')
