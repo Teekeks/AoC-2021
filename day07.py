f3829de1a4c7d8b57e49f08f0f3078cfa3343141
Teekeks <info@teawork.de>
2021-12-08 13:36:19 +0100

    speed go brrrrr

--- day07.py
+++ day07.py
@@ -4,51 +4,14 @@ import time
 inp = [int(x) for x in get_input(7)[0].split(',')]
 t0 = time.perf_counter()
 
-lf = 1000000000000000000000000
-
-
-def get_fuel(t: int):
-    fuel = 0
-    for idx in inp:
-        fuel += abs(idx - t)
-        if fuel > lf:
-            return fuel
-    return fuel
-
-
-for a in range(min(inp), max(inp)):
-    f = get_fuel(a)
-    if f < lf:
-        lf = f
-    else:
-        break
-
+n = len(inp)
+inp = sorted(inp)
+median = inp[int(n/2)]
 print('Part 1:')
-print(lf)
-
-
-lf = 1000000000000000000000000
-
-
-def get_fuel_2(t: int):
-    fuel = 0
-    for idx in inp:
-        steps = abs(idx - t)
-        fuel += int((steps * steps + steps) / 2)
-        if fuel > lf:
-            return fuel
-    return fuel
-
-
-for a in range(min(inp), max(inp)):
-    f = get_fuel_2(a)
-    if f < lf:
-        lf = f
-    else:
-        break
-
+print(sum([abs(i - median) for i in inp]))
+mean = int(sum(inp) / n)
 print('Part 2:')
-print(lf)
+print(min([int(sum([i * (i + 1) / 2 for i in [abs(a - m) for a in inp]])) for m in range(mean - 1, mean + 2)]))
 
 t1 = time.perf_counter() - t0
 print(f'time taken: {t1:.6f}s')
